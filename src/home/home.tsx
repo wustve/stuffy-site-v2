9164e2bae3bdcbbf18a6772d2f740bef8e465163
wustve <wu.stve@gmail.com>
2021-08-29 19:21:45 -0400

    add initial home component

---
+++ src/home/home.tsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+export default class Home extends Component<{stevenStuffy: any, monicaStuffy: any}, {}> {
+    render() {
+        return (
+            <div className = "content">
+                <h1>Stuffy Site</h1>
+                <div id = "stuffyOfTheDayWrapper">
+                    <div className = "stuffyOfTheDay">
+                        <h2>Steven's Stuffy of the Day!</h2>
+                        <h3>{this.props.stevenStuffy.name}</h3>
+                        <img className = "stuffyOfTheDayPic" src = {this.props.stevenStuffy.image}></img>
+                    </div>
+                    <div className = "stuffyOfTheDay">
+                        <h2>Monica's Stuffy of the Day!</h2>
+                        <h3>{this.props.monicaStuffy.name}</h3>
+                        <img className = "stuffyOfTheDayPic" src = {this.props.monicaStuffy.image}> </img>
+                    </div>
+                </div>
+            </div>
+            )
+    }
+}
