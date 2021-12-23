7e3eb249b9d8c685989157d2bb5879e9ab728aa6
msuh1204 <31930969+variablesarecool@users.noreply.github.com>
2021-12-22 23:38:12 -0500

    Make cancel button a component

--- src/stuffyform/StuffyForm.tsx
+++ src/stuffyform/StuffyForm.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import OwnerInput from "./OwnerInput"
 import DeleteButton from "./DeleteButton"
+import CancelButton from "./CancelButton"
 import {ArticleData} from "../../interfaces/ArticleData";
 import {Route, NavLink} from "react-router-dom";
 import { StuffyMenuData } from "../../interfaces/StuffyMenuData";
@@ -8,7 +9,7 @@ import {LocalStorageKey} from "../enums/LocalStorageKey";
 import {ColourMode} from '../enums/ColourMode'
 import '../header/Header.scss';
 
-export default class StuffyForm extends Component<{isAdd: boolean, exit : any, articleData?: ArticleData}> {
+export default class StuffyForm extends Component<{isAdd: boolean, exit? : any, articleData?: ArticleData}> {
 
      constructor(props: any) {
           super(props);
@@ -22,6 +23,12 @@ export default class StuffyForm extends Component<{isAdd: boolean, exit : any, a
 
      generateTextAreaProps(inputName : string) {
           return {id : inputName + "-input", name : inputName};
+     }
+
+     generateCancelButtonProps() {
+          var props = {isAdd : this.props.isAdd};
+          var exitFunc = (typeof this.props.exit !== 'undefined') ? {exit : this.props.exit} : {};
+          return {...props, ...exitFunc};
      }
 
      render() {
@@ -59,7 +66,7 @@ export default class StuffyForm extends Component<{isAdd: boolean, exit : any, a
                          <div id = 'status'></div>
                          <input type = "submit" value = "Submit" id = "submit"/>
                          <DeleteButton isAdd={this.props.isAdd}/>
-                         <button type = "button" onClick = {this.props.exit}>Cancel</button>
+                         <CancelButton {...this.generateCancelButtonProps()}/>
                     </form>
                     <p id = "form-disclaimer"><span style = {{color: "red"}}>*</span> indicates a required field</p>
                </div>
