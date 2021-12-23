c4584f78c6c073668b4a76dbafd56f234210ba85
wustve <wu.stve@gmail.com>
2021-12-23 13:45:20 -0500

    add image dialog

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,7 +1,9 @@
 import { Component } from "react";
 import './home.scss'
+import Image from "../image/image";
+import { StuffyMenuData } from "../../interfaces/StuffyMenuData";
 
-export default class Home extends Component<{stevenStuffy: any, monicaStuffy: any}, {}> {
+export default class Home extends Component<{stevenStuffy: StuffyMenuData, monicaStuffy: StuffyMenuData}, {}> {
     render() {
         return (
             <div id = "homepage">
@@ -10,12 +12,18 @@ export default class Home extends Component<{stevenStuffy: any, monicaStuffy: an
                     <div className = "stuffyOfTheDay">
                         <h2>Steven's Stuffy of the Day!</h2>
                         <h3>{this.props.stevenStuffy.name}</h3>
-                        <img className = "stuffyOfTheDayPic" src = {this.props.stevenStuffy.image}></img>
+                        <div className = "stuffyOfTheDayPic">
+                            <Image src = {this.props.stevenStuffy.image}></Image>
+                        </div>
+                        
                     </div>
                     <div className = "stuffyOfTheDay">
                         <h2>Monica's Stuffy of the Day!</h2>
                         <h3>{this.props.monicaStuffy.name}</h3>
-                        <img className = "stuffyOfTheDayPic" src = {this.props.monicaStuffy.image}></img>
+                        <div className = "stuffyOfTheDayPic">
+                            <Image src = {this.props.monicaStuffy.image}></Image>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
